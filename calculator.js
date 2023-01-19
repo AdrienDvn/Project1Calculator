835c6044c73c64660b7d6a4b43310ff3a8c2aa53
adriendavin <adriendavin@hotmail.fr>
2023-01-19 15:06:47 +0100

    calc finished

--- calculator.js
+++ calculator.js
@@ -1,65 +1,63 @@
-/////LOOP DO WHILE ////////
-// do {
-//   var name1 = prompt("what is your name?");
 
-// }while(name1 == "" || name1 == null)
+//FUNCTIONS
+function addition(num1,num2) {
+    return num1 + num2;
+}
 
-// alert("hello " + name1);
+function substraction(num1,num2) {
+    return num1 - num2;
+}
 
+function multiply(num1,num2) {
+    return num1 * num2;
+}
 
-do {
-  let action = prompt("What calculus do you want to do ?\n\n 1 - Addition\n 2 - Substraction\n 3 - Mutiply\n 4 - Division\n" );
-
-  let num1 = parseInt(prompt("first number?"))
-  let num2 = parseInt(prompt("second number?"))
-
-  if (isNaN(num1) || isNaN(num2)) {
-    throw new Error("Please enter a valid number.");
-  }
-
-  function addition(num1,num2) {
-    result = num1 + num2;
-    return result;
-  };
-
-  function substraction(num1,num2) {
-    result = num1 - num2;
-    return result;
-  };
-
-  function multiply(num1,num2) {
-    result = num1 * num2;
-    return result;
-  };
-
-
-  function division(num1,num2) {
-    result = num1 / num2;
-    return result;
-  };
-
-    switch(action) {
-      case "1":
-        let result = addition(num1,num2);
-        console.log(result);
-        break;
-      case "2":
-        let result = substraction(num1,num2);
-        console.log(result);
-        break;
-      case "3" :
-        let result = multiply(num1,num2);
-        console.log(result);
-        break;
-      case "4" :
-        let result = division(num1,num2);
-        console.log(result);
-        break;
-      default:
-          throw new Error('Please enter choice 1/2/3 or 4.');
+function division(num1,num2) {
+   if(num2 == 0) {
+      throw new Error('not possible to divide by zero.');
     }
+    return num1 / num2;
+}
 
+// ASKING FOR AN ACTION //
+do {
+  var action = Number(prompt("What calculus do you want to do ?\n\n 1 - Addition\n 2 - Substraction\n 3 - Mutiply\n 4 - Division\n" ));
+
+} while(action != 1 && action != 2 && action != 3 && action != 4)
+
+// ASKING FOR A NUMBER //
+do {
+  // ou Number()
+  var num1 = parseInt(prompt("Enter the first number :"))
+  var num2 = parseInt(prompt("Enter the second number :"))
+} while (isNaN(num1) || isNaN(num2))
+
+// CALL THE CHOSEN FUNCTION //
+try {
+   switch(action) {
+        case 1:
+          var result = addition(num1,num2);
+          break;
+
+        case 2:
+          var result = substraction(num1,num2);
+          break;
+
+        case 3 :
+          var result = multiply(num1,num2);
+          break;
+
+        case 4 :
+          var result = division(num1,num2);
+          break;
+
+        default:
+            throw new Error('an error has occured.');
+  }
+  // DISPLAY THE RESULT //
   alert("The answer is " + result);
+}
 
-
-}while(action == "" || action == null)
+  catch(error) {
+    alert(error); // DISPLAY ERROR IF SMTHG WENT WRONG //
+  }
